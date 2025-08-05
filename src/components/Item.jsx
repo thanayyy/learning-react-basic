c1b079c556d7947e3fa3c1264a144ae39d390889
thanayut <thanayyy5998@gmail.com>
2025-08-06 02:52:37 +0700

    last

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import '../css/Item.css'
+import "../css/Item.css";
 //import {useState} from 'react';
-const Item = ({data, deleteStudent}) => {
+const Item = ({ data, deleteStudent }) => {
   return (
     <>
       <li key={data.id} className={data.gender}>
-        <p>
-          {data.id} - {data.name}
-        </p>
+        <p>{data.name}</p>
         <button onClick={() => deleteStudent(data.id)} className="delete">
           ลบ
         </button>
